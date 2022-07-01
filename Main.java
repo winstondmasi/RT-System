b798b96e071f72baa56d1163f5609f41a0bbabc7
Winston Dennis-Masi <winstondennismasi@gmail.com>
2022-07-01 18:09:48 +0100

    Added textfields to the Jframe

--- Main.java
+++ Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         JLabel name = new JLabel();
-        JTextField name_input = new JTextField();
         name.setText("Racer Number 1");//text for racer 1
         name.setHorizontalAlignment(JLabel.LEFT);//alignment is Upward to the left
         name.setVerticalAlignment(JLabel.TOP);
 
         JLabel tyre = new JLabel();
-        JTextField tyre_input = new JTextField();
         tyre.setText("Racer Number 2");//text for racer 2
         tyre.setHorizontalAlignment(JLabel.LEFT);//alignment is to the left
 
         JLabel lap_time = new JLabel();
-        JTextField lap_time_input = new JTextField();
         lap_time.setText("Racer Number 3");//text for racer 3
         lap_time.setHorizontalAlignment(JLabel.LEFT);//alignment is downward to the left
         lap_time.setVerticalAlignment(JLabel.BOTTOM);
 
+        JTextField name_input = new JTextField();
+        JTextField tyre_input = new JTextField();
+        JTextField lap_time_input = new JTextField();
 
         //Creating JFrame
         JFrame window = new JFrame();
@@ -30,11 +30,15 @@ public class Main {
         window.setSize(600, 600);
         window.setResizable(false);
         window.setVisible(true);
-        //window.getContentPane().setBackground(new Color(8, 143, 143));
+        window.getContentPane().setBackground(new Color(128, 128, 128));
 
-        window.add(number_1_label);
-        window.add(number_2_label);
-        window.add(number_3_label);
+        window.add(name);
+        window.add(tyre);
+        window.add(lap_time);
+
+        window.add(name_input);
+        window.add(tyre_input);
+        window.add(lap_time_input);
 
     } 
 

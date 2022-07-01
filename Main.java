6ce0dbe470095f09dff970cb4ad562d460d600b0
Winston Dennis-Masi <winstondennismasi@gmail.com>
2022-07-01 18:02:00 +0100

    Added Horizontal and vertical alignment for the three jpanel variables

--- Main.java
+++ Main.java
@@ -1,28 +1,38 @@
 import java.awt.Color;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
     
     public static void main(String[] args) {
-        
-        //Creating Jframe 
+
+        JLabel number_1_label = new JLabel();
+        number_1_label.setText("Racer Number 1");//text for racer 1
+        number_1_label.setHorizontalAlignment(JLabel.LEFT);//alignment is Upward to the left
+        number_1_label.setVerticalAlignment(JLabel.TOP);
+
+        JLabel number_2_label = new JLabel();
+        number_2_label.setText("Racer Number 2");//text for racer 2
+        number_2_label.setHorizontalAlignment(JLabel.LEFT);//alignment is to the left
+
+        JLabel number_3_label = new JLabel();
+        number_3_label.setText("Racer Number 3");//text for racer 3
+        number_3_label.setHorizontalAlignment(JLabel.LEFT);//alignment is downward to the left
+        number_3_label.setVerticalAlignment(JLabel.BOTTOM);
+
+
+        //Creating JFrame
         JFrame window = new JFrame();
-
         window.setTitle("Race Timing System");
-        
         window.setSize(600, 600);
-
         window.setResizable(false);
-
         window.setVisible(true);
+        //window.getContentPane().setBackground(new Color(8, 143, 143));
 
-        ImageIcon oP_ImageIcon = new ImageIcon("image/Crountch-One-Piece-Jolly-Roger-Luffys-flag.ico");
+        window.add(number_1_label);
+        window.add(number_2_label);
+        window.add(number_3_label);
 
-        window.setIconImage(oP_ImageIcon.getImage());
-
-        window.getContentPane().setBackground(new Color(8, 143, 143));
     } 
 
 }
--- window.java
+++ window.java
@@ -11,19 +11,10 @@ public class window extends JFrame implements ActionListener {
         this.pack();
         this.setVisible(true);
 
+
         JButton jButton = new JButton("Submit Information");
         jButton.addActionListener(this);
-
-        JLabel number_1_label = new JLabel();
-        number_1_label.setText("Racer Number 1");
-
-        JLabel number_2_label = new JLabel();
-        number_2_label.setText("Racer Number 2");
-
-        JLabel number_3_label = new JLabel();
-        number_3_label.setText("Racer Number 3");
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {

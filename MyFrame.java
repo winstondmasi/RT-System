230c0f0e2d0599b8959522984ae870afabf5272d
Winston Dennis-Masi <winstondennismasi@gmail.com>
2022-07-02 17:02:16 +0100

    Set proper alignment for components on JFrame

--- MyFrame.java
+++ MyFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,28 +8,28 @@ public class MyFrame extends JFrame implements ActionListener {
     MyFrame(){
 
         JLabel name = new JLabel();
-        name.setText("What is the racers name?");//name of the racer
-        name.setBounds(500,10, 10,10);
+        name.setText("What is the racers name?\r\n");//name of the racer
+        name.setBounds(25,100, 50,50);
 
         JLabel tyre = new JLabel();
-        tyre.setText("What type of tyre did they use?");//type of tyre
-        tyre.setBounds(350,10, 10,10);
+        tyre.setText("What type of tyre did they use?\r\n");//type of tyre
+        tyre.setBounds(25,250, 50,50);
 
         JLabel lap_time = new JLabel();
-        lap_time.setText("What was the lap time they set (in seconds)?");//lap time
-        lap_time.setBounds(300,10, 10,10);
+        lap_time.setText("What was the lap time they set (in seconds)?\r\n");//lap time
+        lap_time.setBounds(25,400, 10,50);
 
         JTextField name_input = new JTextField();
-        name_input.setBounds(500, 150, 100, 100);
+        name_input.setBounds(300, 100, 250, 75);
 
         JTextField tyre_input = new JTextField();
-        tyre_input.setBounds(350, 150, 100, 100);
+        tyre_input.setBounds(300, 250, 250, 75);
 
         JTextField lap_time_input = new JTextField();
-        lap_time_input.setBounds(200, 150, 100, 100);
+        lap_time_input.setBounds(300, 400, 250, 75);
 
         JButton button = new JButton();
-        button.setBounds(100, 300, 100, 50);
+        button.setBounds(200, 550, 300, 50);
 
         this.setTitle("Race Timing System");
         this.setSize(700, 700);
@@ -36,7 +37,7 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setVisible(true);
         this.setLayout(null);
 
-        //this.getContentPane().setBackground(new Color(128, 128, 128));
+        this.getContentPane().setBackground(new Color(128, 128, 128));
 
         this.add(name);
         this.add(tyre);

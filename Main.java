d24b74710a11a4f2e7d607256dc5fab6404d2835
Winston Dennis-Masi <winstondennismasi@gmail.com>
2022-07-02 16:11:17 +0100

    Added all components to the window

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main{
     public static void main(String[] args) {
 
         //Creating JFrame
-        new JFrame();
+        MyFrame frame = new MyFrame();
 
     }
 
--- MyFrame.java
+++ MyFrame.java
@@ -8,20 +8,30 @@ public class MyFrame extends JFrame implements ActionListener {
 
         JLabel name = new JLabel();
         name.setText("What is the racers name?");//name of the racer
+        name.setBounds(500,10, 10,10);
 
         JLabel tyre = new JLabel();
         tyre.setText("What type of tyre did they use?");//type of tyre
+        tyre.setBounds(350,10, 10,10);
 
         JLabel lap_time = new JLabel();
         lap_time.setText("What was the lap time they set (in seconds)?");//lap time
+        lap_time.setBounds(300,10, 10,10);
 
         JTextField name_input = new JTextField();
-        JTextField tyre_input = new JTextField();
-        JTextField lap_time_input = new JTextField();
+        name_input.setBounds(500, 150, 100, 100);
 
+        JTextField tyre_input = new JTextField();
+        tyre_input.setBounds(350, 150, 100, 100);
+
+        JTextField lap_time_input = new JTextField();
+        lap_time_input.setBounds(200, 150, 100, 100);
+
+        JButton button = new JButton();
+        button.setBounds(100, 300, 100, 50);
 
         this.setTitle("Race Timing System");
-        this.setSize(600, 600);
+        this.setSize(700, 700);
         this.setResizable(false);
         this.setVisible(true);
         this.setLayout(null);
@@ -35,6 +45,8 @@ public class MyFrame extends JFrame implements ActionListener {
         this.add(name_input);
         this.add(tyre_input);
         this.add(lap_time_input);
+
+        this.add(button);
     }
 
     @Override

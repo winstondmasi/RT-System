309273429a588866e41409e250b02ca27ce794bc
Winston Chukwugozilam Dennis-Masi <wd21708@essex.ac.uk>
2022-12-15 23:01:48 +0000

    Update

--- MyFrame.java
+++ MyFrame.java
@@ -1,178 +1,46 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.*;
 import javax.swing.JTextField;
 
-/*
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.table.*;
+public class MyFrame extends JFrame{
+    JFrame frame = new JFrame("Window Example");
 
-import javax.swing.border.LineBorder;
- */
-public class MyFrame extends JFrame implements ActionListener {
+    // Set the size of the window
+        frame.setSize(400, 200);
 
-    JButton button;
+    // Set the default close operation (what happens when the user clicks the X button)
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JTextField name_input;
-    JTextField tyre_input;
-    JTextField lap_time_input;
+    // Create a panel to hold the fields and the button
+    JPanel panel = new JPanel();
 
-    JTable jTable;
-    JPanel mainPanel;
+    // Create the three fields and add them to the panel
+    JTextField nameField = new JTextField("Name", 20);
+    JTextField tyreField = new JTextField("Tyre", 20);
+    JTextField lapTimeField = new JTextField("Lap Time", 20);
+        panel.add(nameField);
+        panel.add(tyreField);
+        panel.add(lapTimeField);
 
-    MyFrame(){
+    // Create the submit button and add it to the panel
+    JButton submitButton = new JButton("Submit");
+        panel.add(submitButton);
 
-        JLabel name = new JLabel();
-        name.setText("Name: \r\n");//name of the racer
-        name.setBounds(25,100, 70,50);
+    // Add the panel to the window
+        frame.add(panel);
 
-        JLabel tyre = new JLabel();
-        tyre.setText("Tyre: \r\n");//type of tyre
-        tyre.setBounds(25,250, 70,50);
+    // Show the window
+        frame.setVisible(true);
 
-        JLabel lap_time = new JLabel();
-        lap_time.setText("Time: \r\n");//lap time
-        lap_time.setBounds(25,400, 70,50);
-
-        name_input = new JTextField();
-        name_input.setBounds(250, 100, 250, 75);
-
-        tyre_input = new JTextField();
-        tyre_input.setBounds(250, 250, 250, 75);
-
-        lap_time_input = new JTextField();
-        lap_time_input.setBounds(250, 400, 250, 75);
-
-
-        ImageIcon imageIcon = new ImageIcon("image/point.png"); // load the image to a imageIcon
-        Image image = imageIcon.getImage(); // transform it 
-        Image newimg = image.getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-        imageIcon = new ImageIcon(newimg);  // transform it back
-
-        button = new JButton();
-        button.setBounds(200, 550, 300, 50);
-        button.addActionListener(this);
-        button.setText("Submit");
-        button.setIcon(imageIcon);
-        button.setFont(new Font("Ubuntu", Font.PLAIN, 20));
-
-        this.setTitle("Race Timing System");
-        this.setSize(700, 700);
-        this.setResizable(false);
-        this.setVisible(true);
-        this.setLayout(null);
-
-        this.getContentPane().setBackground(new Color(128, 128, 128));
-
-        this.add(name);
-        this.add(tyre);
-        this.add(lap_time);
-
-        this.add(name_input);
-        this.add(tyre_input);
-        this.add(lap_time_input);
-
-        this.add(button);
-    }
-
-
-    public void clear() {
-        String EMPTY_STRING = "";
-        name_input.setText(EMPTY_STRING);
-        tyre_input.setText(EMPTY_STRING);
-        lap_time_input.setText(EMPTY_STRING);
-    }
-
-    public static boolean containsItemFromArray(JTextField tyre_input2, String[] tyre_used) {
-        // Convert the array of String items as a Stream
-        // For each element of the Stream call inputString.contains(element)
-        // If you have any match returns true, false otherwise
-
-            for(int i =0; i < tyre_used.length; i++){
-                if(tyre_input2.getText().contains(tyre_used[i])){
-                return true;
-            }
+    // Add an action listener to the submit button
+submitButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // When the submit button is pressed, add the values in the fields to the table
+            Table.addEntry(nameField.getText(), tyreField.getText(), lapTimeField.getText());
         }
-        return false;
-    }
+    });
 
-    //creating the table 2).
-    public void table(){
-
-        JFrame tablFrame = new JFrame();
-        final String[] tableDataHeadings = {"name", "Tyre", "Lap time"};
-        final String[][] tableData = {{name_input.getText(), tyre_input.getText(), lap_time_input.getText()}};
-
-        /*
-        model = new DefaultTableModel(tableData,tableDataHeadings);
-        model.addColumn("Name");
-        model.addColumn("Tyre Used");
-        model.addColumn("Lap Time Elapsed");
-         */
-        
-        //creating table
-        jTable = new JTable (tableData,tableDataHeadings);
-
-        jTable.setBounds(100, 100, 350, 350);
-
-        JScrollPane jScrollPane = new JScrollPane(jTable);
-        
-        tablFrame.setTitle("Table");
-        tablFrame.setSize(350, 300);
-        tablFrame.add(jScrollPane);
-        tablFrame.pack();
-        tablFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tablFrame.setVisible(true);
-        tablFrame.setResizable(true);
-
-    }
-
-    /* 
-    public void addRowToTable(){
-        
-        model.addRow(new Object[]{
-            name_input.getText(), 
-            tyre_input.getText(),
-            lap_time_input.getText()
-            });
-    }
-    */
-    
-    @Override
-    public void actionPerformed(ActionEvent e){
-
-        String[] tyre_used = {"Hard", "Medium", "Soft", "Intermediate", "Wet"};
-
-
-        if (e.getSource() == button)
-        {
-            if(name_input != null && tyre_input != null && lap_time_input != null && containsItemFromArray(tyre_input, tyre_used))
-            {
-                System.out.println(name_input.getText() + " " 
-                + tyre_input.getText() + " " 
-                + lap_time_input.getText() + "s");
-
-                table();
-            
-                clear(); 
-
-            }
-            else{
-                System.out.println("Not a valid.\n(The types of tyres are Hard, Medium, Soft, Intermediate and Wet) ");
-            }
-            
-        }
-    }
 }
---
+++ Table.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Table {
+    // The table data will be stored in a two-dimensional array
+    private static String[][] data;
+
+    // The column names will be stored in an array
+    private String[] columnNames = {"Name", "Tyre", "Lap Time"};
+
+    // The table will be created with a JTable object
+    private static JTable table;
+
+    // The scroll pane will be used to add scrolling to the table
+    private JScrollPane scrollPane;
+
+    public Table() {
+        // Initialize the data array with enough rows to hold 10 entries
+        data = new String[10][3];
+
+        // Create a new JTable with the data array and the column names array
+        table = new JTable(data, columnNames);
+
+        // Create a new scroll pane with the table as its content
+        scrollPane = new JScrollPane(table);
+
+        // Add the scroll pane to a panel so it can be added to a window
+        JPanel panel = new JPanel();
+        panel.add(scrollPane);
+
+        // Create a new window and add the panel with the table to it
+        JFrame frame = new JFrame("Table Example");
+        frame.add(panel);
+
+        // Set the size of the window and show it
+        frame.setSize(400, 200);
+        frame.setVisible(true);
+    }
+
+    // This method adds a new entry to the table
+    public static void addEntry(String name, String tyre, String lapTime) {
+        // Find the first empty row in the table
+        int row = 0;
+        while (row < data.length && data[row][0] != null) {
+            row++;
+        }
+
+        // If the table is full, do nothing
+        if (row >= data.length) {
+            return;
+        }
+
+        // Add the entry to the table
+        data[row][0] = name;
+        data[row][1] = tyre;
+        data[row][2] = lapTime;
+
+        // Update the table
+        table.updateUI();
+    }
+}
+
